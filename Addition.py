9110dd0dc3ecb6d5dad29d912eb465f3ad74110b
CodeMK889 <148438596+CodeMK889@users.noreply.github.com>
2023-10-31 15:09:21 +0530

    Add files via upload

---
+++ Addition.py
@@ -0,0 +1,20 @@
+# def addition(x,y):
+#     add = x + y
+#     print(add)
+# addition(4,5)
+
+# def addition():
+#     x = 10
+#     y = 15
+#     c = x + y
+#     print(c)
+# addition()
+
+def addition(y):
+    x = 10
+    c = x + y
+    print(c)
+addition(13)
+
+
+
